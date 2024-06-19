c87e069b354983bd3232ddffc4adec3f2e94c27e
Manj0t <Manjotssandhu1@gmail.com>
2024-06-18 22:32:58 -0700

    Changes to grid class

--- Summer Project/Assets/Grid.cs
+++ Summer Project/Assets/Grid.cs
@@ -11,48 +11,51 @@ using UnityEngine.InputSystem.iOS.LowLevel;
 using UnityEngine.Tilemaps;
 using UnityEngine.UI;
 
-public class Grid{
+public class Grid<TGridObject>{
     private int width;
     private int height;
     private float cellSize;
-    private int[,] gridArray;
+    private TGridObject[,] gridArray;
     private TextMesh[,] debugTextArry;
+    private Vector3 originPosition;
 
-    public Grid(int width, int height, float cellSize){
+    public Grid(int width, int height, float cellSize, Vector3 originPosition){
         this.width = width;
         this.height = height;
         this.cellSize = cellSize;
+        this.originPosition = originPosition;
 
-        gridArray = new int[width, height];
+        gridArray = new TGridObject[width, height];
         debugTextArry = new TextMesh[width, height];
-        //Debug.DrawLine draws the grid lines
-        for(int x = 0; x < gridArray.GetLength(0); x++){
-            for(int y = 0; y < gridArray.GetLength(1); y++){
-                debugTextArry[x, y] = CreateWorldText(gridArray[x, y].ToString(), null, GetWorldPosition(x, y) + new Vector3(cellSize, cellSize) * 0.5f, 2, Color.white, TextAnchor.MiddleCenter);
-                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
-                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
+        //Debug.DrawLine draws the grid lines\
+        bool showDebug = true;
+        if(showDebug){        
+            for(int x = 0; x < gridArray.GetLength(0); x++){
+                for(int y = 0; y < gridArray.GetLength(1); y++){
+                    debugTextArry[x, y] = CreateWorldText(gridArray[x, y].ToString(), null, GetWorldPosition(x, y) + new Vector3(cellSize, cellSize) * 0.5f, 2, Color.white, TextAnchor.MiddleCenter);
+                    Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+                    Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
+                }
             }
+            Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+            Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
         }
-        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
-        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
-
-        SetValue(2, 1, 56);
     }
     private Vector3 GetWorldPosition(int x, int y){
-        return new Vector3(x, y) * cellSize;
+        return new Vector3(x, y) * cellSize + originPosition;
     }
     private void GetXY(Vector3 worldPosition, out int x, out int y){
-        x = Mathf.FloorToInt(worldPosition.x / cellSize);
-        y = Mathf.FloorToInt(worldPosition.y / cellSize);
+        x = Mathf.FloorToInt((worldPosition - originPosition).x / cellSize);
+        y = Mathf.FloorToInt((worldPosition - originPosition).y / cellSize);
     }
-    public void SetValue(int x, int y, int value){
+    public void SetValue(int x, int y, TGridObject value){
         if(x >= 0 && y >= 0 && x < width && y < height){
             gridArray[x, y] = value;
             debugTextArry[x, y].text = gridArray[x, y].ToString();
         }
     }
 
-    public void SetValue(Vector3 worldPosition, int value){
+    public void SetValue(Vector3 worldPosition, TGridObject value){
         int x, y;
         GetXY(worldPosition, out x, out y);
         SetValue(x, y, value);
--- Summer Project/Assets/Testing.cs
+++ Summer Project/Assets/Testing.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.SceneManagement;
+using UnityEditor.U2D.Aseprite;
 using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
     // Start is called before the first frame update
     const float CELLSIZE = 0.16f;
+    Grid<int> grid;
+    GameObject[] enemies;
     void Start()
     {
-        Grid grid = new Grid(10, 10, CELLSIZE);
+        enemies = GameObject.FindGameObjectsWithTag("Slime");
+        foreach(GameObject enemy in enemies){
+            Vector3 pos = enemy.transform.position;
+            grid = new Grid<int>(10, 10, CELLSIZE, pos - new Vector3(5 * CELLSIZE, 5 * CELLSIZE));
+        }
     }
 
     // Update is called once per frame
     private void Update()
     {
+        
+        // enemy = GameObject.FindGameObjectWithTag("Player").transform.position;
+        // grid = new Grid<int>(10, 10, CELLSIZE, enemy);
     }
 }
